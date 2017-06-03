fa3750d2d5f2a0b8f18abb9d09148878eba09c88
Junn Dobit Paras <junndobit123@gmail.com>
2017-06-03 15:59:08 +0800

    back up daw

--- app/src/main/java/com/example/dobit/recall/RecordActivity.java
+++ app/src/main/java/com/example/dobit/recall/RecordActivity.java
@@ -21,6 +21,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -30,10 +31,20 @@ import java.util.Locale;
 public class RecordActivity extends AppCompatActivity implements View.OnClickListener {
 
     ImageView record;
+    String time;
+    String days;
+    String weeks;
+    String addHours;
+    long dayToMs = 86400000;
+    long HourToMs = 3600000;
+    long mils;
     int hours;
     int minutes;
-    FirebaseDatabase fbdb;
-    DatabaseReference dbr;
+    DatabaseReference databaseNotes;
+    DateFormat dateFormat;
+    Calendar calendar;
+    Date date;
+    String dateString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,9 +52,7 @@ public class RecordActivity extends AppCompatActivity implements View.OnClickLis
         setContentView(R.layout.activity_record);
         record = (ImageView) findViewById(R.id.ivRecord);
         record.setOnClickListener(this);
-
-        fbdb = FirebaseDatabase.getInstance();
-        dbr = fbdb.getReference().child("notes");
+        databaseNotes = FirebaseDatabase.getInstance().getReference("notes");
 
     }
 
@@ -69,38 +78,82 @@ public class RecordActivity extends AppCompatActivity implements View.OnClickLis
         super.onActivityResult(requestcode, result_code, i);
         switch (requestcode) {
             case 100:
-                DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-                Date date = new Date();
+                dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+                date = new Date();
 
                 if (result_code == RESULT_OK && i != null) {
                     ArrayList<String> result = i.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
                     String rec = result.get(0).toString();
                     String[] split = rec.split(" ");
+                    String[] timeSplit;
                     Intent alarmIntent = new Intent(AlarmClock.ACTION_SET_ALARM);
 
-                if(rec.matches(".*\\d+.*")) {
+                    //If the record has a number, get its time
+                    if(rec.matches(".*\\d+.*") && !rec.contains("tomorrow") && !rec.contains("days from now") && !rec.contains("weeks from now")) {
+                        for (int count = 0; count < split.length; count++) {
 
-                    for (int count = 0; count < split.length; count++){
-                        if(split[count].matches("[0-9]")){
-                            hours = Integer.parseInt(split[count]);
-                        }else if(split[count].matches("[0-60]")){
-                            minutes = Integer.parseInt(split[count]);
+                            if (split[count].contains(":") && rec.contains("p.m.") || rec.contains("a.m.")) {
+                                time = split[count];
+                            } else if (split[count].matches(".*\\d+.*") && rec.contains("p.m.") || rec.contains("a.m.")) {
+                                time = split[count];
+                            }
+
+                            if(split[count].contains("days") && rec.contains("days from now")){
+                                days = split[count-1];
+                            }
+
+                            if(split[count].contains("weeks") && rec.contains("week from now")){
+                                days = split[count-1];
+                            }
+
+                            if(split[count].contains("hours") && rec.contains("hours from now")){
+                                addHours = split[count-1];
+                            }
                         }
+
+                        //If the time string has ':' then split it and get the hours and minutes, else get only the hours, there is no minutes
+                        if (time.contains(":")) {
+                            timeSplit = time.split(":");
+                            hours = Integer.parseInt(timeSplit[0]);
+                            minutes = Integer.parseInt(timeSplit[1]);
+                        } else{
+                            hours = Integer.parseInt(time);
+                            minutes = 0;
+                        }
+
+
+                        if (rec.contains("PM") || rec.contains("p.m.")) {
+                            if (hours != 12)
+                                hours = hours + 12;
+                        } else if (rec.contains("AM") || rec.contains("a.m.")) {
+                            if (hours == 12)
+                                hours = 0;
+                            else
+                                hours = hours + 0;
+                        }
+
+
+                        alarmIntent.putExtra(AlarmClock.EXTRA_SKIP_UI, true); //Don't let the user get into the alarm app
+                        alarmIntent.putExtra(AlarmClock.EXTRA_HOUR, hours);
+                        alarmIntent.putExtra(AlarmClock.EXTRA_MINUTES, minutes);
+                        alarmIntent.putExtra(AlarmClock.EXTRA_MESSAGE, rec);
+                        startActivity(alarmIntent); //Set alarm
                     }
 
-                }
-                    alarmIntent.putExtra(AlarmClock.EXTRA_SKIP_UI, true);
-                    alarmIntent.putExtra(AlarmClock.EXTRA_HOUR, hours);
-                    alarmIntent.putExtra(AlarmClock.EXTRA_MINUTES, minutes);
-                    if (rec.contains("PM") || rec.contains("p.m.")) {
-                        alarmIntent.putExtra(AlarmClock.EXTRA_IS_PM, true);
-                    } else if (rec.contains("AM") || rec.contains("a.m.")) {
-                        alarmIntent.putExtra(AlarmClock.EXTRA_IS_PM, false);
+
+                    //For plotting calendar purposes
+                    calendar = Calendar.getInstance();
+                    dateString = (calendar.get(Calendar.YEAR) + "/" + (calendar.get(Calendar.MONTH)+1) + "/" + calendar.get(Calendar.DAY_OF_MONTH) + " " + hours + ":" + minutes + ":" + "00");
+
+                    //Parse date then convert date to milliseconds
+                    Date parseDate = new Date();
+                    try {
+                        parseDate = dateFormat.parse(dateString);
+                    } catch (ParseException e) {
+                        e.printStackTrace();
                     }
+                    mils = parseDate.getTime();
 
-
-                    alarmIntent.putExtra(AlarmClock.EXTRA_MESSAGE, rec);
-                    startActivity(alarmIntent);
 
 
                     ContentResolver cr = this.getContentResolver();
@@ -108,8 +161,23 @@ public class RecordActivity extends AppCompatActivity implements View.OnClickLis
                     cv.put(CalendarContract.Events.TITLE, dateFormat.format(date));
                     cv.put(CalendarContract.Events.DESCRIPTION, rec);
                     cv.put(CalendarContract.Events.EVENT_LOCATION, "N/A");
-                    cv.put(CalendarContract.Events.DTSTART, Calendar.getInstance().getTimeInMillis());
-                    cv.put(CalendarContract.Events.DTEND, Calendar.getInstance().getTimeInMillis() + 60 * 60 * 1000);
+
+                    //Future events
+                    if(rec.contains("tomorrow") || rec.contains("a day from now")) {
+                        cv.put(CalendarContract.Events.DTSTART, mils + dayToMs);
+                        cv.put(CalendarContract.Events.DTEND, mils + dayToMs);
+                    }else if(rec.contains("next week") || rec.contains("a week from now")){
+                        cv.put(CalendarContract.Events.DTSTART, mils + (dayToMs*7));
+                        cv.put(CalendarContract.Events.DTEND, mils + (dayToMs*7));
+                    }else if(rec.contains("an hour from now") || rec.contains("in an hour")){
+                        cv.put(CalendarContract.Events.DTSTART, mils + HourToMs);
+                        cv.put(CalendarContract.Events.DTEND, mils + HourToMs);
+                    }else{
+                        cv.put(CalendarContract.Events.DTSTART, mils);
+                        cv.put(CalendarContract.Events.DTEND, mils);
+                    }
+                    //
+
                     cv.put(CalendarContract.Events.CALENDAR_ID, 1);
                     cv.put(CalendarContract.Events.EVENT_TIMEZONE, Calendar.getInstance().getTimeZone().getID());
 
@@ -117,12 +185,12 @@ public class RecordActivity extends AppCompatActivity implements View.OnClickLis
                         return;
                     }
 
-
+                    //Push notes to Firebase DB
+                    String id = databaseNotes.push().getKey();
                     Notes notes = new Notes();
                     notes.setDate(dateFormat.format(date));
                     notes.setNote(rec);
-                    dbr.setValue(notes);
-                    dbr.push();
+                    databaseNotes.child(id).setValue(notes);
 
 
                     Uri uri = cr.insert(CalendarContract.Events.CONTENT_URI, cv);
